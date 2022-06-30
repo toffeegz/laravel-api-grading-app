01253c8057ac1b87a292cfd9b9c2ea134504c242
gezrylclariz <gezrylclarizg@gmail.com>
2022-06-30 23:18:29 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\ProfileController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\RoleController;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('login', LoginController::class);
+    Route::post('logout', LogoutController::class);
+    Route::get('profile', ProfileController::class);
 });
+
+Route::resource('/roles', RoleController::class);
+Route::resource('/users', UserController::class);
