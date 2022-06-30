60ea38c9b4db830a6b1dee7c6b7474a6a6df9b3a
gezrylclariz <gezrylclarizg@gmail.com>
2022-06-30 20:17:35 +0800

    UserFactory and RoleFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use App\Models\User;
 
 class UserFactory extends Factory
 {
@@ -15,8 +16,13 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
+            'last_name' => $this->faker->lastName(),
+            'first_name' => $this->faker->firstName($gender = null|'male'|'female'),
+            'middle_name' => $this->faker->lastName(),
+            'code' => $this->faker->ean8(),
             'email' => $this->faker->unique()->safeEmail(),
+            'role_id' => rand(1,5),
+            'profile_photo_path' => $this->faker->image($dir, $width, $height, 'cats', false),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
