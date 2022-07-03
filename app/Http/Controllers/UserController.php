7bdc02bd826f18617db4720d8a4cb75bff086146
gezrylclariz <gezrylclarizg@gmail.com>
2022-07-03 22:43:40 +0800

    UserController: crud function with auth

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Http\Requests\UserRequest;
+
 use Illuminate\Http\Request;
+use Hash;
 
 class UserController extends Controller
 {
@@ -12,10 +15,32 @@ class UserController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+    
     public function index()
     {
-        //
+        $users = User::all();
+        foreach($users as $user)
+        {
+            $data[] = [
+                
+                'last_name' => $user->last_name,
+                'first_name' => $user->first_name,
+                'middle_name' => $user->middle_name,
+                'code' => $user->code,
+                'profile_photo_path' => $user->profile_photo_path,
+                'email' => $user->email,
+                'role' => $user->role,
+                
+            ];
+        }
+        return response()->json($data);
     }
+
 
     /**
      * Show the form for creating a new resource.
@@ -33,9 +58,20 @@ class UserController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UserRequest $userRequest, User $user)
     {
-        //
+        $data = User::create([
+            'last_name' => $userRequest->last_name,
+            'first_name' => $userRequest->first_name,
+            'middle_name' => $userRequest->middle_name,
+            'code' => $userRequest->code,
+            'role_id' => $userRequest->role_id,
+            'profile_photo_path' => $userRequest->profile_photo_path,
+            'email' => $userRequest->email,
+            'password' =>  Hash::make($userRequest->password),
+        ]);
+        $data->role = $data->role;
+        return response()->json(['message' => 'User has been added successfully', 'data' => $data]);
     }
 
     /**
@@ -67,9 +103,29 @@ class UserController extends Controller
      * @param  \App\Models\User  $user
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, User $user)
+    public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'last_name' => 'nullable|string|max:255',
+            'first_name' => 'nullable|string|max:255',
+            'middle_name' => 'nullable|string|max:255',
+            'profile_photo_path' => 'nullable|string|max:255',
+            'code' => 'nullable|string|max:255|unique:users,code,'.$id,
+            'email' => 'nullable|string|email|max:255|unique:users,email,'.$id,
+            'password' => 'nullable|string|min:8|confirmed',
+            'role_id' => 'nullable|numeric'
+        ]);
+
+        $data = User::find($id);
+        $data->fill($request->except(['password']));
+        if($request->has('password'))
+        {
+            $data->password = Hash::make($request->password);
+        }
+        $data->save();
+        $data->role = $data->role;
+
+        return response()->json(['message' => 'User has been updated successfully', 'data' => $data]);
     }
 
     /**
@@ -80,6 +136,7 @@ class UserController extends Controller
      */
     public function destroy(User $user)
     {
-        //
+        $user->delete();
+        return response()->json(['message' => 'User has been removed successfully']);
     }
 }
