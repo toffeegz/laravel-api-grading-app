989801e60d8b8a039dbde0a2417367ac45056bee
gezrylclariz <gezrylclarizg@gmail.com>
2022-07-04 19:31:38 +0800

    Create PermissionSeeder.php

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Permission;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Permission::factory()->count(10)->create();
+    }
+}
