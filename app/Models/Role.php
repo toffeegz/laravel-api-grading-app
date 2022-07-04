2de341ede4b5728821d2fcf49feff41e59757fa7
gezrylclariz <gezrylclarizg@gmail.com>
2022-07-04 15:07:04 +0800

    Update Role.php
    user relationship, fillable, guarded, timestamps = true

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Laratrust\Models\LaratrustRole;
+use App\Models\User;
 
 class Role extends LaratrustRole
 {
-    public $guarded = [];
+    public $timestamps = true;
+
+    protected $guarded = [];
+
+    protected $fillable = [
+        'name', 
+        'display_name', 
+        'description',
+    ];
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
 }
